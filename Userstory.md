19630fc0857315f667a2ce7a1c49c1d91a95ee0c
blor15 <blor15@hotmail.com>
2021-09-21 19:45:47 -0600

    Added start and reset buttons, score history, and cleaned up code by making objects

--- Userstory.md
+++ Userstory.md
@@ -1,12 +1,12 @@
 As a user playing the game I want to:
 
-start the game by pressing a Start button
-use my arrow keys to change the direction of the snake
-have the snake grow correctly when it eats the apple
-have the game end if the snake tries to eat itself
-have the game end if the snake runs into a wall
-see how long my snake was when the game ended
-start the game over without having to reset the browser
+start the game by pressing a Start button 
+use my arrow keys to change the direction of the snake *
+have the snake grow correctly when it eats the apple *
+have the game end if the snake tries to eat itself *
+have the game end if the snake runs into a wall *
+see how long my snake was when the game ended *
+start the game over without having to reset the browser 
 As a user playing the game I would be delighted if I:
 
 can set the difficulty (speed of snake)
--- app.js
+++ app.js
@@ -1,121 +1,146 @@
-const board_border = 'black';
-const board_background = "slategrey";
-const snake_col = 'green';
-const snake_border = 'black'
+const colors = {
+    black: 'black',
+    green: 'green',
+    red: 'red',
+    slategrey: 'slategrey',
+};
 
-const apple_col = 'red'
-const apple_border = 'black'
+const color_applications = {
+    board_border: colors.black,
+    board_background: colors.slategrey,
+    snake_color: colors.green,
+    snake_border: colors.black,
+    apple_color: colors.red,
+    apple_border: colors.black,
+};
 
+const keys = {
+    ARROW_LEFT: 37,
+    ARROW_RIGHT: 39,
+    ARROW_UP: 38,
+    ARROW_DOWN: 40,
+}
+const gameState = {
+    horizontalVelocity: 10,
+    verticalVelocity: 0,
+    appleX: null,
+    appleY: null,
+    score: 0,
+    snake: [
+        {x: 200, y: 200},
+        {x: 190, y: 200}
+    ]
+}
 
-let dx = 10;
-let dy = 0;
-let apple_x;
-let apple_y;
-let score = 0;
-
-let snake = [
-    {x: 200, y: 200},
-    {x: 190, y: 200},
-    {x: 180, y: 200},
-    {x: 170, y: 200},
-    {x: 160, y: 200},
-];
 const gameboard = document.getElementById('gameboard');
 
 const gameboard_ctx = gameboard.getContext("2d");
 
-main();
+const startButton = document.getElementById('start');
+
+const resetButton = document.getElementById('reset');
+
+const scoreHistory = document.getElementById('score-history');
 
 document.addEventListener('keydown', direction)
 
-generate_apple();
-
-function main() {
-
-    if (gameover()) return;
-
-    setTimeout(function onTick() {
-        clearCanvas();
-        drawApple();
-        move_snake();
-        drawSnake();
-        main();
-    }, 100)
+function onTick() {
+    clearCanvas();
+    drawApple();
+    move_snake();
+    drawSnake();
+    runGameplayLoop();
 }
-// Initial game state
+function runGameplayLoop() {
+    if (gameover()) {
+        const li = document.createElement('li');
+        
+        li.textContent = document.getElementById('score').textContent;
+
+        scoreHistory.appendChild(li);
+
+        document.removeEventListener('keydown', direction);
+
+        return alert(`Snake length = ${gameState.snake.length}`);
+    
+    }
+
+    setTimeout(onTick, 100);
+}
+
+
 function clearCanvas () {
-    gameboard_ctx.fillStyle = board_background;
-    gameboard_ctx.strokestyle = board_border;
+    gameboard_ctx.fillStyle = color_applications.board_background;
+    gameboard_ctx.strokestyle = color_applications.board_border;
     gameboard_ctx.fillRect(0, 0, gameboard.width, gameboard.height);
     gameboard_ctx.strokeRect(0, 0, gameboard.width, gameboard.height);
 }
 
 function drawSnakePart (snakePart) {
-    gameboard_ctx.fillStyle = snake_col;
-    gameboard_ctx.strokestyle = snake_border;
+    gameboard_ctx.fillStyle = color_applications.snake_color
+    gameboard_ctx.strokestyle = color_applications.snake_border;
     gameboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
     gameboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
 function drawSnake () {
-    snake.forEach(drawSnakePart);
+    gameState.snake.forEach(drawSnakePart);
 }
 
 // Render movement
 function move_snake () {
-    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
-    snake.unshift(head);
-    const ate_food = snake[0].x === apple_x && snake[0].y === apple_y;
+    const head = {
+        x: gameState.snake[0].x + gameState.horizontalVelocity, 
+        y: gameState.snake[0].y + gameState.verticalVelocity
+    };
+    gameState.snake.unshift(head);
+    const ate_food = gameState.snake[0].x === gameState.appleX && gameState.snake[0].y === gameState.appleY;
     if (ate_food) {
-        score += 1;
-        document.getElementById('score').innerHTML = score;
-        generate_apple;
+        gameState.score += 1;
+        document.getElementById('score').innerHTML = gameState.score;
+        generate_apple();
     } else {
-    snake.pop();
+    gameState.snake.pop();
     }
 } 
 
-// setTimeout(function onTick() { clearCanvas(); move_snake(); drawSnake();}, 100);
 
 function direction(event) {
-    const ArrowLeft = 37;
-    const ArrowRight = 39;
-    const ArrowUp = 38;
-    const ArrowDown = 40;
-
     const keyPressed = event.keyCode;
-    const up = dy === -10;
-    const down = dy === 10;
-    const right = dx === 10;
-    const left = dx === -10;
+    const up = gameState.verticalVelocity === -10;
+    const down = gameState.verticalVelocity === 10;
+    const right = gameState.horizontalVelocity === 10;
+    const left = gameState.horizontalVelocity === -10;
 
- if (keyPressed === ArrowLeft && !right) {
-     dx = -10;
-     dy= 0;
+ if (keyPressed === keys.ARROW_LEFT && !right) {
+     gameState.horizontalVelocity = -10;
+     gameState.verticalVelocity = 0;
  }
- if (keyPressed === ArrowUp && !down) {
-     dx = 0;
-     dy = -10;
+ if (keyPressed === keys.ARROW_UP && !down) {
+     gameState.horizontalVelocity = 0;
+     gameState.verticalVelocity = -10;
  }
- if (keyPressed === ArrowRight && !right) {
-     dx = 10;
-     dx = 0;
+ if (keyPressed === keys.ARROW_RIGHT && !left) {
+     gameState.horizontalVelocity = 10;
+     gameState.verticalVelocity = 0;
  }
- if (keyPressed === ArrowDown && !up) {
-     dx = 0;
-     dy = 10;
+ if (keyPressed === keys.ARROW_DOWN && !up) {
+     gameState.horizontalVelocity = 0;
+     gameState.verticalVelocity = 10;
  }
 }
 
 function gameover() {
-    for (let i = 4; i < snake.length; i++) {
-        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) 
-        return true
+    for (let i = 2; i < gameState.snake.length; i++) {
+        if (gameState.snake[i].x === gameState.snake[0].x && 
+            gameState.snake[i].y === gameState.snake[0].y) 
+        return true;
     }
-    const hitLeft = snake[0].x < 0;
-    const hitRight = snake[0].x > gameboard.width - 10;
-    const hitTop = snake[0].y < 0;
-    const hitBottom = snake[0].y > gameboard.height - 10;
+
+    const hitLeft = gameState.snake[0].x < 0;
+    const hitRight = gameState.snake[0].x > gameboard.width - 10;
+    const hitTop = gameState.snake[0].y < 0;
+    const hitBottom = gameState.snake[0].y > gameboard.height - 10;
     return hitLeft || hitRight || hitTop || hitBottom
 }
 
@@ -124,18 +149,45 @@ function spawn_apple(min, max) {
 }
 
 function generate_apple () {
-    apple_x = spawn_apple(0, gameboard.width - 10);
-    apple_y = spawn_apple(0, gameboard.height - 10);
-    snake.forEach (function apple_ate(part) {
-        const ate = part.x == apple_x && part.y == apple_y;
-        if (ate) spawn_apple();
+    gameState.appleX = spawn_apple(0, gameboard.width - 10);
+    gameState.appleY = spawn_apple(0, gameboard.height - 10);
+    gameState.snake.forEach(function apple_ate(part) {
+        const ate = part.x == gameState.appleX && part.y == gameState.appleY; 
+            if (ate) return spawn_apple();
     });
 }
 
 function drawApple () {
-    gameboard_ctx.fillStyle = 'red'
-    gameboard_ctx.strokestyle = 'black';
-    gameboard_ctx.fillRect(apple_x, apple_y, 10, 10);
-    gameboard_ctx.strokeRect(apple_x, apple_y, 10, 10);
+    gameboard_ctx.fillStyle = color_applications.apple_color
+    gameboard_ctx.strokestyle = color_applications.apple_border
+    gameboard_ctx.fillRect(gameState.appleX, gameState.appleY, 10, 10);
+    gameboard_ctx.strokeRect(gameState.appleX, gameState.appleY, 10, 10);
 }
 
+function startGame() {
+    document.addEventListener("keydown", direction);
+    
+    generate_apple();
+    runGameplayLoop();
+}
+
+
+function initializeGame() {
+    startButton.addEventListener('click', startGame);
+}
+
+function resetGame() {
+    clearCanvas();
+    drawApple();
+    drawSnake();
+    move_snake();
+    generate_apple();
+    document.addEventListener('keydown', direction)
+}
+
+function reset() {
+    resetButton.addEventListener('click', resetGame);
+}
+
+initializeGame();
+reset();
